2c9f2ea262f8323b427e9a9763f72f48e59e4caf
Szymon <szymon.skwierzynski@gmail.com>
2013-11-13 20:49:21 +0100

    new LPDictionary class

---
+++ InteligentnyTraktor/InteligentnyTraktor.LanguageProcessing/LPDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InteligentnyTraktor.Model;
+
+namespace InteligentnyTraktor.LanguageProcessing
+{
+    class LPDictionary
+    {
+        public Dictionary<String, Enum> Dict;
+
+        LPDictionary()
+        {
+            Dict = new Dictionary<String, Enum>();
+
+            Dict.Add("Idz", TractorTaskType.Move);
+            Dict.Add("Idź", TractorTaskType.Move);
+            Dict.Add("Rusz", TractorTaskType.Move);
+
+        }
+    }
+}
